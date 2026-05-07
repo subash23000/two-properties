a4284fff03c0b85c0c9d91fc931fec08a55dc281
Subash Chhetri <156183094+Subashchhetri946@users.noreply.github.com>
2026-05-07 12:45:08 +0300

    updated

--- src/app/LegoApp.java
+++ src/app/LegoApp.java
@@ -7,12 +7,18 @@ public class LegoApp {
 	public static void main(String[] args) {
 		RunLego runLego=new RunLego();
 		ReadData readData=new ReadData();
+		SendData sendData=new SendData();
+
+		Thread sendDataThread = new Thread(sendData);
+		sendDataThread.start(); // Start the thread to send data to the server
 
 		System.out.println("Run in Threads");
 
 		Thread runLegoThread=new Thread(runLego);
 		Thread readDataThread=new Thread(readData);
+		Thread sendDataThread=new Thread(sendData);
 		runLegoThread.start(); //Saikeen kaynnistys
 		readDataThread.start(); //Virtuaalikone aloittaa saikeen kun ehtii
+		sendDataThread.start(); // Start the thread to send data to the server
 	}
 }
---
+++ src/threads/SendData.java
@@ -0,0 +1,31 @@
+package threads;
+
+import data.Robot;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class SendData implements Runnable {
+
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(2000);
+
+                URL url = new URL("http://10.22.16.206:8080/rest/lego/setvalues/"
+                        + Robot.getRun() + "/"
+                        + Robot.getSpeed() + "/"
+                        + Robot.getTurn());
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.getInputStream();
+                conn.disconnect();
+
+                System.out.println("Sent data to server");
+
+            } catch (Exception e) {
+                System.out.println("Cannot send data");
+            }
+        }
+    }
+}
